4249e9a021578a3a32d7f083c3bb7569dc68b5fb
liz <liz.mtzgr.00@gmail.com>
2023-10-10 13:32:00 -0500

    yay the light is green

--- ankle_control/src/tiva_test.c
+++ ankle_control/src/tiva_test.c
@@ -3,8 +3,9 @@
 /// Designed for use with the EK-TM4C123GXL board
 #include "nuhal/error.h"
 #include "nuhal/tiva.h"
-#include "nuhal/time"
+#include "nuhal/time.h"
 #include "nuhal/led.h"
+#include "ankle_control/encoder.h"
 
 /// \brief Flash the leds infinitely
 int main(void)
@@ -12,4 +13,5 @@ int main(void)
     tiva_setup();
     led_setup();
     led_set(LED_COLOR_GREEN);
+    return 0;
 }
