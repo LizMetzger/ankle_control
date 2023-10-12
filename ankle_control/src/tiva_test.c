47157d032832ed00b706ef2140cbb6116029cdf7
liz <liz.mtzgr.00@gmail.com>
2023-10-12 12:00:31 -0500

    when a button is pushed the led on the board changes colors and a message is sent to screen that says 'push'

--- ankle_control/src/tiva_test.c
+++ ankle_control/src/tiva_test.c
@@ -4,21 +4,53 @@
 #include "nuhal/error.h"
 #include "nuhal/tiva.h"
 #include "nuhal/time.h"
+#include "nuhal/uart.h"
+#include "nuhal/utilities.h"
+#include "nuhal/uart_tiva.h"
 #include "nuhal/led.h"
+#include "nuhal/pin_tiva.h"
+#include "driverlib/uart.h"
+#include"driverlib/interrupt.h"
+#include"inc/hw_ints.h"
+#include"inc/hw_memmap.h"
 #include "ankle_control/encoder.h"
+
+#define FSR PIN('A',2)
+#define BUTTON PIN('A',3)
+static const struct pin_configuration pins[] =
+{
+    // sensor peripherals
+    {FSR, PIN_INPUT},
+    {BUTTON, PIN_INPUT},
+    // servo
+    // uart pins
+    {GPIO_PA0_U0RX,  PIN_UART},
+    {GPIO_PA1_U0TX, PIN_UART}
+};
 
 /// \brief Flash the leds infinitely
 int main(void)
 {
     tiva_setup();
+    pin_setup(pins, ARRAY_LEN(pins));
     led_setup();
-    dummy_func();
+    // open UART communication
+    const struct uart_port * port =
+        uart_open("0", 1000000, UART_FLOW_NONE, UART_PARITY_NONE);
+
+    const uint8_t data[] = "push \n";
     for(;;)
     {
-        led_set(LED_COLOR_GREEN);
-        time_delay_ms(1000);
-        led_set(LED_COLOR_BLUE);
-        time_delay_ms(1000);
+        if(pin_read(BUTTON))
+        {
+            led_set(LED_COLOR_RED);
+            uart_write_block(port, &data, strlen(data), 0);
+            time_delay_ms(100);
+        }
+        else
+        {
+            led_set(LED_COLOR_BLUE);
+        }
     }
     return 0;
 }
